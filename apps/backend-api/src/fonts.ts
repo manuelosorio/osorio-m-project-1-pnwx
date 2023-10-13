393245efd44a25efe9d7fc7330d0dad2cfd1d088
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2023-10-12 20:35:03 -0400

    Create font cdn

---
+++ apps/backend-api/src/fonts.ts
@@ -0,0 +1,98 @@
+import { Router } from 'express';
+import cors from 'cors';
+
+const fontsRouter = Router();
+
+// Base types
+type FontStyle = 'normal' | 'ital';
+type FontWeight =
+  | '100'
+  | '200'
+  | '300'
+  | '400'
+  | '500'
+  | '600'
+  | '700'
+  | '800'
+  | '900';
+
+// A single font variant can be a combination of a style and weight
+type FontVariant = {
+  style: FontStyle;
+  weight: FontWeight;
+};
+
+// A font family can have multiple variants
+type FontFamily = {
+  name: string;
+  variants: FontVariant[];
+};
+
+// Families type representing an array of font families
+type Families = Array<FontFamily>;
+
+function parseFontFamilies(family: string[]): Families {
+  const families: Families = [];
+
+  for (const familyStr of family) {
+    const [fontName, allVariants] = familyStr.split(':');
+    const variantsStrings = allVariants.split(';');
+
+    const variants: FontVariant[] = [];
+
+    for (let variantStr of variantsStrings) {
+      let style: FontStyle = 'normal';
+      let weight: FontWeight = '400';
+
+      if (variantStr.includes('ital,')) {
+        style = 'ital';
+        variantStr = variantStr.replace('ital,', ''); // remove 'ital,' from the string
+      }
+
+      if (variantStr.includes('wght@')) {
+        weight = variantStr.split('wght@')[1] as FontWeight;
+      }
+
+      variants.push({ style, weight });
+    }
+
+    families.push({ name: fontName, variants });
+  }
+  return families;
+}
+
+fontsRouter.get('/', cors(), (req, res) => {
+  if (!req.query.family) {
+    return res.status(400).send('Error: "family" query parameter is required.');
+  }
+
+  let parsedFamilies: Families;
+  try {
+    parsedFamilies = parseFontFamilies(req.query.family as string[]);
+  } catch (error) {
+    return res
+      .status(400)
+      .send(`Error parsing "family" parameter: ${error.message}`);
+  }
+
+  let cssContent = '';
+
+  for (const family of parsedFamilies) {
+    console.log(family);
+    for (const variant of family.variants) {
+      const fontPath = `/static/fonts/${family.name}/${family.name}-${variant.style}-${variant.weight}.woff`;
+      cssContent += `
+@font-face {
+  font-family: "${family.name}";
+  src: url(${fontPath}) format('woff');
+  font-weight: ${variant.weight};
+  font-style: ${variant.style === 'ital' ? 'italic' : 'normal'};
+}`;
+    }
+  }
+
+  res.setHeader('Content-Type', 'text/css');
+  res.send(cssContent);
+});
+
+export default fontsRouter;
--- apps/backend-api/src/main.ts
+++ apps/backend-api/src/main.ts
@@ -1,14 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+
+import fontsRouter from './fonts';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
 const app = express();
+app.use('/static', cors(), express.static('public'));
+app.use('/fonts', fontsRouter);
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ ready ] \\  ${host}:${port}`);
 });
