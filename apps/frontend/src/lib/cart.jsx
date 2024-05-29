c9eb5d7a61d88c6a8453176c64b1f09edb0510d6
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-05-28 20:02:22 -0400

    Update cart page, add loader

--- apps/frontend/src/lib/cart.jsx
+++ apps/frontend/src/lib/cart.jsx
@@ -1,10 +1,18 @@
 import { useState, useEffect } from 'react';
 
+function cartItem() {
+  return {
+    margin: 'auto',
+  };
+}
+
 function Cart(props) {
   const [cartData, setCartData] = useState(null);
 
   useEffect(() => {
+    console.log('useEffect');
     async function fetchData() {
+      console.log('fetching data');
       try {
         const urlRoute = import.meta.env.PUBLIC_API_URL + '/cart';
         const response = await fetch(urlRoute, {
@@ -12,24 +20,40 @@ function Cart(props) {
           credentials: 'include',
         });
         const data = await response.json();
+        console.log('data', data);
+
         setCartData(data);
       } catch (error) {
         console.error('Error fetching cart data:', error);
       }
     }
-
-    fetchData();
+    fetchData().then();
   }, []);
 
-  return (
-    <div>
-      {cartData ? (
-        <div>Cart data: {JSON.stringify(cartData)}</div>
-      ) : (
-        <div>Loading cart data...</div>
-      )}
-    </div>
-  );
+  if (!cartData) {
+    return (
+      <div className="loading">
+        <div className="loading__circle"></div>
+        <div className="loading__circle"></div>
+        <div className="loading__circle"></div>
+      </div>
+    );
+  }
+  if (cartData.items.length === 0) {
+    return <div>Cart is empty.</div>;
+  }
+  return cartData.items.map((item) => {
+    return (
+      <div key={item.id} className="product-card cart-item" style={cartItem()}>
+        <div className="cart-item__image">
+          <img src={item.image} alt={item.name} />
+        </div>
+        <div className="cart-item__info">
+          <div>{item.name}</div>
+          <div>{item.price}</div>
+        </div>
+      </div>
+    );
+  });
 }
-
 export default Cart;
